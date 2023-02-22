088a2a0d1f1d43e571c0f7487b2019aaa1443971
cled227 <cled227@uky.edu>
2023-02-22 16:15:11 -0500

    Added basic button functionality

--- src/TrackerGUI.java
+++ src/TrackerGUI.java
@@ -1,47 +1,68 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TrackerGUI extends JFrame {
-    TaskList tList = new TaskList();
+public class TrackerGUI extends JFrame implements ActionListener {
+    TaskList tList;
+    JPanel taskPanel;
+
     public TrackerGUI(String wName) {
         super(wName);
+        tList = new TaskList();
+        taskPanel = new JPanel();
+        add(taskPanel, BorderLayout.NORTH);
     }
 
-    public JPanel updateTaskPanel(){
-        JPanel tPanel = new JPanel();
+    public void updateTaskPanel(){
+        JLabel display = new JLabel();
 
         if(tList.taskNames.size() == 0){
-            JLabel empty = new JLabel("There are now tasks yet, try adding one!");
-            tPanel.add(empty);
+            System.out.println("no tasks");
+            display.setText("There are now tasks yet, try adding one!");
         }
         else {
+            System.out.println("tasks exist");
+            StringBuilder toDisplay = new StringBuilder();
             for (int i = 0; i < tList.taskNames.size(); i++) {
-                JLabel text = new JLabel(tList.taskNames.get(i));
-                tPanel.add(text);
+                toDisplay.append(tList.taskNames.get(i) + '\n');
+                System.out.println("Added task: " + tList.taskNames.get(i));
             }
+            display.setText(toDisplay.toString());
         }
-        return tPanel;
+        //invalidate();
+        //validate();
+        //repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String action = ae.getActionCommand();
+        if (action.equals("newTask")) {
+            String newTask = JOptionPane.showInputDialog("Task Name: ");
+            tList.addTask(newTask);
+            updateTaskPanel();
+        }
     }
 
     public static void main(String[] args) {
         TrackerGUI frame = new TrackerGUI("Daily Tracker");
-        frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel taskPanel = frame.updateTaskPanel();
+        frame.updateTaskPanel();
 
         JPanel buttonPanel = new JPanel();
 
         JButton addTaskButton = new JButton("Add Task");
+        addTaskButton.addActionListener(frame);
+        addTaskButton.setActionCommand("newTask");
         JButton removeTaskButton = new JButton("Remove Task");
 
         buttonPanel.add(addTaskButton);
         buttonPanel.add(removeTaskButton);
-        //panel.setBackground(Color.GRAY.darker());
 
-
-
-        frame.add(taskPanel);
-        frame.add(buttonPanel);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.pack();
         frame.setVisible(true);
     }
 }
