c008c74642e8e2b7090ecd0f49b79f7937e0c8b6
CLEdmundson <logan.edmundson@gmail.com>
2023-03-06 23:29:40 -0500

    Added integrated saving and loading methods for tasks Added strikethrough to completed tasks Added exit confirmation prompt

--- src/TaskList.java
+++ src/TaskList.java
@@ -1,47 +1,59 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskList {
     final int Max_Task_Length = 30;
+    final String FILENAME = "Saved_Tasks";
     ArrayList<JCheckBox> tasks;
 
-    public TaskList(){
+    public TaskList() {
         tasks = new ArrayList<>();
     }
 
     //Adds a new task, assumes the task is incomplete by default
-    public void addTask(String tn) {
+    public void addTask(String tn, boolean check) {
         int bufferLength = Max_Task_Length - tn.length();
         String bufferedtn = tn + (new String(new char[bufferLength]).replace('\0', ' '));
-        JCheckBox cBox = new JCheckBox(bufferedtn, false);
+        JCheckBox cBox = new JCheckBox(bufferedtn, check);
         cBox.setHorizontalTextPosition(SwingConstants.LEFT);
         cBox.setMargin(new Insets(0, 20, 0, 100));
         tasks.add(cBox);
     }
 
+    public void addTask(String tn) {
+        addTask(tn, false);
+    }
+
     /**
      * Checks if TaskList contains a task with name tn
-     * @param tn        name of the task
+     *
+     * @param tn name of the task
      * @return boolean value
      */
     public boolean isTask(String tn) {
         boolean ans = false;
-        for (JCheckBox cBox: tasks)
-            if(cBox.getText().equals(tn))
+        for (JCheckBox cBox : tasks)
+            if (cBox.getText().equals(tn))
                 ans = true;
-        if(!ans)
+        if (!ans)
             System.out.println("No task with such a name exists.");
         return ans;
     }
 
     /**
      * Edit a preexisting task in the TaskList
-     * @param tn        name of the task to be edited
-     * @param action    action to be performed on the task
-     *                  1: remove
-     *                  2: finish
-     *                  3: reset
+     *
+     * @param tn     name of the task to be edited
+     * @param action action to be performed on the task
+     *               1: remove
+     *               2: finish
+     *               3: reset
      */
     public void editTask(String tn, int action) {
         if (isTask(tn)) {
@@ -58,28 +70,74 @@ public class TaskList {
         }
     }
 
-    public void printTaskList(){
-        for (JCheckBox cBox: tasks)
+    public void printTaskList() {
+        for (JCheckBox cBox : tasks)
             System.out.println("Task: " + cBox.getText() + ", Complete: " + cBox.isSelected());
     }
 
+    public void createSaveFile() {
+        System.out.println("File does not exist, creating file...");
+        File f = new File(FILENAME);
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            f.createNewFile();
+            System.out.println("File created: " + f.getName());
+        } catch (IOException e) {
+            System.out.println("Error Creating File...");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void loadTasks() {
+        // TODO: Check if day has passes and reset complete status if so
+        File f = new File(FILENAME);
+        try {
+            Scanner myReader = new Scanner(f);
+            while (myReader.hasNextLine()) {
+                String[] data = myReader.nextLine().split(",");
+                addTask(data[0], Boolean.parseBoolean(data[1]));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            createSaveFile();
+        }
+    }
+
+    public void saveTasks() {
+        try {
+            FileWriter myWriter = new FileWriter(FILENAME);
+
+            for (JCheckBox cBox: tasks){
+                myWriter.write(cBox.getText() + "," + cBox.isSelected() + "\n");
+            }
+            myWriter.close();
+            System.out.println("Successfully saved to file.");
+        } catch (IOException e) {
+            System.out.println("Error saving to file...");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
+        // TODO: Add method and inline comments
         TaskList tList = new TaskList();
 
         // Add Tasks
-        tList.addTask("Brush Teeth (Morning)");
-        tList.addTask("Take Meds");
-        tList.addTask("Take Meds");
-        tList.addTask("Take Meds (dupe)");
-        tList.addTask("Brush Teeth (Night)");
+        tList.addTask("Task 1");
+        tList.addTask("Task 2");
+        tList.addTask("Task 2");
+        tList.addTask("Task 3");
+        tList.addTask("Task 4");
 
         // Test Functions
         tList.printTaskList();
-        tList.editTask("Take Meds", 2);
-        tList.editTask("Take Meds (dupe)", 2);
+        tList.editTask("Task 2", 2);
+        tList.editTask("Task 3", 2);
         tList.printTaskList();
-        tList.editTask("Take Meds (dupe)", 1);
-        tList.editTask("f", 1);
+        tList.editTask("Task 3", 1);
+        tList.editTask("Fake Task", 1);
+
         tList.printTaskList();
     }
 }
--- src/TrackerGUI.java
+++ src/TrackerGUI.java
@@ -3,46 +3,57 @@ import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
+import java.awt.font.TextAttribute;
 import java.util.Enumeration;
+import java.util.Map;
 
 public class TrackerGUI extends JFrame implements ActionListener, ItemListener {
-    TaskList tList;
+    static TaskList tList;
     JPanel taskPanel;
     JCheckBox lastClicked;
-
 
     public TrackerGUI(String wName) {
         super(wName);
         tList = new TaskList();
+        tList.loadTasks();
         taskPanel = new JPanel();
         taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));
         add(taskPanel, BorderLayout.NORTH);
     }
 
     public void updateTaskPanel() {
-        // Clean the unupdated tasklist
+        // Clean the old taskList
         taskPanel.removeAll();
 
-        JLabel display = new JLabel("");
-
         if (tList.tasks.size() == 0) {
-            display.setText("There are no tasks yet, try adding one!");
+            JLabel display = new JLabel("There are no tasks yet, try adding one!");
 
             Border border = display.getBorder();
             Border margin = new EmptyBorder(2, 20, 2, 44);
             display.setBorder(new CompoundBorder(border, margin));
             taskPanel.add(display, BorderLayout.WEST);
         } else {
-            for (JCheckBox cBox: tList.tasks) {
+            for (JCheckBox cBox : tList.tasks) {
+                setStatusFont(cBox);
                 cBox.addActionListener((TrackerGUI) SwingUtilities.windowForComponent(taskPanel));
                 taskPanel.add(cBox);
-                System.out.println("Added task: " + cBox.getText());
             }
         }
+
+        revalidate();
+        repaint();
+        pack();
+    }
+
+    public void setStatusFont(JCheckBox cBox){
+        // TODO: Fix warnings around the attributes Map
+        // TODO: Make completed status more visual i.e. font color
+
+        @SuppressWarnings("unchecked")
+        Map<TextAttribute, Boolean> attributes = (Map<TextAttribute, Boolean>) cBox.getFont().getAttributes();
+        attributes.put(TextAttribute.STRIKETHROUGH, cBox.isSelected());
+        cBox.setFont(new Font(attributes));
 
         revalidate();
         repaint();
@@ -56,8 +67,10 @@ public class TrackerGUI extends JFrame implements ActionListener, ItemListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() instanceof JCheckBox)
-            lastClicked = (JCheckBox) ae.getSource();
+        if (ae.getSource() instanceof JCheckBox cBox) {
+            lastClicked = cBox;
+            setStatusFont(cBox);
+        }
         String action = ae.getActionCommand();
         if (action.equals("newTask")) {
             String newTask = JOptionPane.showInputDialog("Task Name: ");
@@ -70,21 +83,34 @@ public class TrackerGUI extends JFrame implements ActionListener, ItemListener {
         }
     }
 
-    public static void setUIFont(javax.swing.plaf.FontUIResource f)
-    {
+    public static void setUIFont(javax.swing.plaf.FontUIResource f) {
         Enumeration<Object> keys = UIManager.getDefaults().keys();
-        while(keys.hasMoreElements())
-        {
+        while (keys.hasMoreElements()) {
             Object key = keys.nextElement();
             Object value = UIManager.get(key);
-            if(value instanceof javax.swing.plaf.FontUIResource) UIManager.put(key, f);
+            if (value instanceof javax.swing.plaf.FontUIResource) UIManager.put(key, f);
         }
     }
 
     public static void main(String[] args) {
-        TrackerGUI frame = new TrackerGUI("Daily Tracker");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUIFont(new javax.swing.plaf.FontUIResource(Font.MONOSPACED, Font.BOLD, 14));
+
+        TrackerGUI frame = new TrackerGUI("Daily Tracker");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showOptionDialog(
+                        null, "Are You Sure to Close Application?",
+                        "Exit Confirmation", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (confirm == 0) {
+                    tList.saveTasks();
+                    System.exit(0);
+                }
+            }
+        });
+
 
         frame.updateTaskPanel();
 
